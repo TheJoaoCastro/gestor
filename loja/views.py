d9d40297189595eff4a3bc05f3b4c029bd59f250
TheJoaoCastro <theferenciado@gmail.com>
2025-01-11 15:10:56 -0300

    dev: add dashboard render with perms

--- loja/views.py
+++ loja/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def dashboard(request):
-    return render(request, 'loja/dashboard.html')
+    if request.user.has_perm("loja.delete_loja"):
+        return render(request, 'loja/dashboard-ceo.html')
+    elif request.user.has_perm("loja.change_loja"):
+        return render(request, 'loja/dashboard-gerente.html')
+    else:
+        return render(request, 'loja/dashboard-vendedor.html')
