56d33f5e6ef7931516273bd7160aeb66e03f9628
TheJoaoCastro <theferenciado@gmail.com>
2025-01-25 16:22:17 -0300

    feat: add model of dashboards for ceo, manager and employee

--- loja/models.py
+++ loja/models.py
@@ -1,19 +1,26 @@
 from django.db import models
+from accounts.models import Funcionario
 
 class Loja(models.Model):
-    id_gerente = models.IntegerField(default=0)
+    id_gerente = models.ForeignKey(Funcionario, on_delete=models.DO_NOTHING)
     endereco = models.CharField(max_length=200)
     altura = models.DecimalField(decimal_places=1, max_digits=2)
     largura = models.DecimalField(decimal_places=1, max_digits=4)
     profundidade = models.DecimalField(decimal_places=1, max_digits=4)
     
+    def __str__(self):
+        return 'Loja ' + str(self.id) + ', ' + self.endereco
+    
 class DadosProduto(models.Model):
     nome = models.CharField(max_length=300)
-    peso = models.DecimalField(decimal_places=2, max_digits=4)
-    altura = models.DecimalField(decimal_places=1, max_digits=2)
-    largura = models.DecimalField(decimal_places=1, max_digits=2)
-    profundidade = models.DecimalField(decimal_places=1, max_digits=2)
+    peso = models.IntegerField()
+    altura = models.IntegerField()
+    largura = models.IntegerField()
+    profundidade = models.IntegerField()
     fator_empilhamento = models.IntegerField()
+    
+    def __str__(self):
+        return self.nome
 
 class ProdutoLoja(models.Model):
     id_loja = models.ForeignKey(Loja, on_delete=models.CASCADE)
@@ -23,3 +30,6 @@ class ProdutoLoja(models.Model):
     qnt_vendas = models.IntegerField()
     ultima_venda = models.DateField(auto_now=True)
     id_dados_produto = models.ForeignKey(DadosProduto, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return 'Produto: ' + self.id_dados_produto.nome + ', Loja: ' + self.id_loja.endereco
--- loja/views.py
+++ loja/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import ProdutoLoja
 
 def dashboard(request):
     if request.user.has_perm("loja.delete_loja"):
         return render(request, 'loja/dashboard-ceo.html')
     elif request.user.has_perm("loja.change_loja"):
-        return render(request, 'loja/dashboard-gerente.html')
+        produtos = ProdutoLoja.objects.filter(id_loja__id=request.user.id_loja)
+        context = {'produtos': produtos}
+        return render(request, 'loja/dashboard-gerente.html', context)
     else:
-        return render(request, 'loja/dashboard-vendedor.html')
+        produtos = ProdutoLoja.objects.filter(id_loja__id=request.user.id_loja)
+        context = {'produtos': produtos}
+        return render(request, 'loja/dashboard-vendedor.html', context)
